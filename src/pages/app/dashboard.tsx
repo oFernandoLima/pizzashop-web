4aa2ebd8ecdd1f0d04bd649889bbcf4503b9970d
Fernando Lima <eufernandoprogramador@gmail.com>
2024-02-03 03:27:20 -0300

    feat: applying SEO with Helmet

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,6 +1,9 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Dashboard() {
   return (
     <div className="flex h-screen items-center justify-center">
+      <Helmet title="Dashboard" />
       <h1>Dashboard</h1>
     </div>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,9 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
   return (
     <div className="flex h-screen items-center justify-center">
+      <Helmet title="Login" />
       <h1>SignIn</h1>
     </div>
   )
