791a39224d63756b41196fd02bf0effac4c3c988
Fernando Lima <eufernandoprogramador@gmail.com>
2024-02-27 17:47:56 -0300

    feat: search and display of order list

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 
+import { getOrders } from '@/api/get-orders'
 import { Pagination } from '@/components/pagination'
 import {
   Table,
@@ -12,9 +14,12 @@ import {
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
 
-export interface OrdersProps {}
+export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders,
+  })
 
-export function Orders(props: OrdersProps) {
   return (
     <>
       <Helmet title="Pedidos" />
@@ -38,9 +43,10 @@ export function Orders(props: OrdersProps) {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
-                  return <OrderTableRow key={i} />
-                })}
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
+                  })}
               </TableBody>
             </Table>
           </div>
