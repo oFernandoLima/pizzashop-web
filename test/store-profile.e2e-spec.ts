f9268703ee1c91d37c3d0c265f8b9a4a34d9e24c
Fernando Lima <eufernandoprogramador@gmail.com>
2024-03-01 01:52:35 -0300

    fix: inserting await in test lines

--- test/store-profile.e2e-spec.ts
+++ test/store-profile.e2e-spec.ts
@@ -15,9 +15,11 @@ test('update profile successfully', async ({ page }) => {
 
   const toast = page.getByText('Perfil atualizado com sucesso!')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 
   await page.getByRole('button', { name: 'Close' }).click()
 
-  expect(page.getByRole('button', { name: 'Pizza do Byano' })).toBeVisible()
+  await expect(
+    page.getByRole('button', { name: 'Pizza do Byano' }),
+  ).toBeVisible()
 })
